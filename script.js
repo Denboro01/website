a56d7920e0cb35b3f098a928260f38ff885a60c9
Denis Borodin <90827835+Denboro01@users.noreply.github.com>
2026-05-09 18:22:55 +0100

    Attempt to fix the image delay bug

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const displayText = document.getElementById("displaytext");
 const displayImages = ['images/Denis_Display.png', 'images/Fourly_Display.png', 'images/Other_Half_Display.png', 'images/Adorabullet_Display.png', 'images/SoF_Display.png'];
 const gameNames = ['Who is Denis?', 'Fourly', 'Other Half', 'Adorabullet', 'Signs of Forest'];
+const gameArt = document.getElementById("gameart");
+const display = document.getElementsByClassName("display")[0];
+
+// Attempting to fix the delayed image change bug
+const preloadedImages = [];
+displayImages.forEach(src => {
+    const img = new Image();
+    img.src = src;
+    preloadedImages.push(img);
+})
+
 
 function UpdateDisplay(id) {
-    // Change the image of GameArt
-    document.getElementById("gameart").src = displayImages[id];
+    // Change the image of GameArt by using the preloaded images
+    gameArt.src = preloadedImages[id].src;
 
     // Change the DisplayText's text
     displayText.innerHTML = gameNames[id];
@@ -18,7 +29,7 @@ function AnimationDelay() {
         games[i].style.animationDelay = String(i) + "s";
 
         // Add an event listener that triggers the popin for both the gameart and display name
-        games[i].addEventListener("mouseover", PopIn);
+        games[i].addEventListener("mouseenter", PopIn);
     }
 
     let extpages = document.getElementsByClassName('extpage');
@@ -31,7 +42,7 @@ function AnimationDelay() {
 function PopIn() {
 
     // Animate the game art popin
-     document.getElementById("gameart").animate([
+     gameArt.animate([
          {
              transform: "scale(0)"
          },
@@ -43,7 +54,7 @@ function PopIn() {
      });
 
     // Animate the display name popin
-    document.getElementsByClassName("display")[0].animate([
+    display.animate([
         {
             transform: "scale(0)"
         },
@@ -57,7 +68,7 @@ function PopIn() {
         duration: 300
     });
 
-    document.getElementsByClassName("display")[0].style.transform = "scale(1)";
+    display.style.transform = "scale(1)";
 }
 
 AnimationDelay();
